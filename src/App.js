4c885348be8a34a37ab7978b06d2cff70cd0d69d
faiz <faizanahmad72560@gmail.com>
2022-03-22 23:16:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import React, { Component } from 'react'
+// import Navbar from './components/Navbar';
+// import News from './components/News';
+// import Newsitem from './components/Newsitem';
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//             <Navbar/>   
+//             <News/>    
+//             <Newsitem/>    
+
+//       </div>
+//     )
+//   }
+// }
+
+
+import React from 'react'
+import Temp from './components/temp'
+// import Weathercard from './components/weathercard'
+
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Temp/>
+      
+    </div>
+  )
+}
+
+export default App
